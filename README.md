4a3c1d37582ed67bff709c2af39df367d5cc9219
mohaymenrafi <mohaymen.rafi@gmail.com>
2022-05-31 00:11:41 +0600

    two number sum added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # problem-solving
+
+### purpose of this thread this to keep track of my solved problems in github.
---
+++ two-number-sum.js
@@ -0,0 +1,21 @@
+// sample input [3, 5, -4, 8, 11, 1, -1, 6] target sum  = 10
+// sample output [-1 ,11]
+
+function twoNumberSum(arr, targetSum) {
+    const numbers = {};
+    for(let x of arr) {
+        const y = targetSum - x;
+        if(y in numbers) {
+            return [ x, y]
+        } else {
+            numbers[y] = true;
+        }
+    }
+    return []
+}
+
+const res = twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10)
+console.log(res)
+
+
+
