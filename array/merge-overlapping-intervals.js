ae720b1b37d16db84733a1c6d8081c4d26a99946
mohaymenrafi <mohaymen.rafi@gmail.com>
2022-06-15 21:51:37 +0600

    merge overlapping completed

--- array/merge-overlapping-intervals.js
+++ array/merge-overlapping-intervals.js
@@ -13,5 +13,19 @@
 // ]
 
 function mergeOverlappingIntervals(array) {
+    sortedArray = array.sort((a,b) => a[0] - b[0])
+    const prevPair = sortedArray[0];
+    const mergedArray = [prevPair];
     
+    for(let pair of sortedArray){
+        const [ _ , prevPairEnd] = prevPair;
+        const [currentPairStart, currentPairEnd] = pair
+        if(prevPairEnd < currentPairStart) {
+            prevPair = pair
+            mergedArray.push(prevPair)
+        } else {
+            prevPair[1] = Math.max(prevPairEnd, currentPairEnd)
+        }
+    }
+    return mergedArray;
 }
